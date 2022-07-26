6c0cb5062b720119bec89a55cc6412e69ff1e59b
GunseiKPaseri <GunseiKPaseri@gmail.com>
2022-07-26 15:36:48 +0900

    remove count property

--- type.d.ts
+++ type.d.ts
@@ -4,10 +4,6 @@ interface detectResult {
   japaneseCharacters: string
   chineseCharacters: string
   otherCharacters: string
-  allCount: number
-  japaneseCount: number
-  chineseCount : number
-  otherCount : number
 }
 export function detect(text: string): detectResult
 
